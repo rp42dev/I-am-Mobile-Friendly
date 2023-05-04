f990aa84af81c28a090945dac953e6c274e0543b
Raivis Petrovskis <rapet80@gmail.com>
2023-05-04 19:34:11 +0100

    add popup message

--- backend/main/views.py
+++ backend/main/views.py
@@ -7,6 +7,7 @@ from rest_framework import viewsets
 from .serializers import MySerializer, EmailSerializer
 from .models import Project
 
+
 def index(request):
     pass
     return render(request, 'index.html')
@@ -19,7 +20,7 @@ def transform(request):
 class ProjectsViewSet(viewsets.ModelViewSet):
     serializer_class = MySerializer
     queryset = Project.objects.all()
-    
+
 
 class SendEmailViewSet(viewsets.ViewSet):
     serializer_class = EmailSerializer
@@ -31,24 +32,21 @@ class SendEmailViewSet(viewsets.ViewSet):
         name = serializer.validated_data['name']
         from_email = serializer.validated_data['fromEmail']
         message = serializer.validated_data['message']
-        
+
         send_mail(
-            subject = 'I am mobile friendly Contact Form Submission',
-            message = f'Name: {name}\nEmail: {from_email}\nMessage: {message}',
-            from_email = self.email,
-            recipient_list = [self.email],
+            subject='I am mobile friendly Contact Form Submission',
+            message=f'Name: {name}\nEmail: {from_email}\nMessage: {message}',
+            from_email=self.email,
+            recipient_list=[self.email],
         )
         send_mail(
             subject=f"Thank you for contacting me, {name}",
-            message=f"Thank you for contacting me, {name}." 
+            message=f"Thank you for contacting me, {name}."
                     f"I will get back to you as soon as possible.\n\n"
                     f"Best regards,\n"
                     f"Raivis from I am mobile friendly",
-                    
+
             from_email=self.email,
             recipient_list=[from_email],
         )
         return JsonResponse({'message': 'Email sent successfully'})
-    
-    
-    
