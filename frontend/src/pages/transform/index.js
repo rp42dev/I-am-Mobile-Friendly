3de118024f02463cb403f9a7c791a9f53af3bab3
Raivis Petrovskis <rapet80@gmail.com>
2023-05-04 14:43:06 +0100

    fix spaceing

--- frontend/src/pages/transform/index.js
+++ frontend/src/pages/transform/index.js
@@ -17,7 +17,7 @@ const Transform = (props) => {
                 <div className="mt-8 container">
                     <div className="max-w-full hero-content flex-col xl:flex-row-reverse justify-between mx-auto p-0">
                         <div className="max-w-2xl w-full h-full">
-                            <div className="flex justify-center px-4 py-8 xl:py-0 shadow-inherit max-h-screen">
+                            <div className="flex justify-center px-0 xl-px-4 py-4 xl:py-0 shadow-inherit max-h-screen">
                                 <img src={image} alt="Hero image" className="hero-img object-contain h-100 w-full 2xl:px-20" />
                             </div>
                         </div>
