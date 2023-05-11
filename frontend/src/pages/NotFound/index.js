35a3b9c5c6ce413d9db26d0f05b51654c9cc7102
Raivis Petrovskis <rapet80@gmail.com>
2023-05-11 23:10:18 +0100

    add 404 page

--- frontend/src/pages/NotFound/index.js
+++ frontend/src/pages/NotFound/index.js
@@ -1,11 +1,24 @@
+import { useLocation } from 'react-router-dom';
 const NotFound = () => {
+    const location = useLocation();
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold">404</h1>
-            <h2 className="text-2xl font-bold">Page Not Found</h2>
-            <p className="text-xl">Sorry, this page does not exist</p>
-            <a href="/" className="text-blue-500 hover:underline">Go back to the homepage</a>
+
+        <div class="relative h-screen overflow-hidden">
+         
+            <div class="absolute inset-0">
+            </div>
+            <div class="container relative z-10 flex items-center px-6 py-32 mx-auto md:px-12 xl:py-40">
+                <div class="relative z-10 flex flex-col items-center w-full font-mono">
+                    <h1 class="mt-4 text-5xl font-extrabold leading-tight text-center">
+                        You&#x27;re alone here
+                    </h1>
+                    <p class="font-extrabold  text-8xl my-44 animate-bounce">
+                        404
+                    </p>
+                </div>
+            </div>
         </div>
+
     )
 }
 
