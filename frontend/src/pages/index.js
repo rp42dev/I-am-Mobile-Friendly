608015420d55ac99c445d7c5686d80afa4d3e4d0
Raivis Petrovskis <rapet80@gmail.com>
2024-05-09 20:19:27 +0100

    restoring

--- frontend/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import About from "./about";
-import Budget from "./budget";
-import Custom from "./custom";
-import Home from "./home";
-import NotFound from "./NotFound";
-import Success from "./success";
-import Funnels from "./Funnels";
-
-export { About, Budget, Custom, Home, NotFound, Success, Funnels };
