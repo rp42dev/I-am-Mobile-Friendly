42b9587287c35e5c7c7bb6395f172f270108e15c
Raivis Petrovskis <rapet80@gmail.com>
2024-05-11 14:56:31 +0100

    added changes to many things

--- frontend/src/components/ChatBot.js
+++ frontend/src/components/ChatBot.js
@@ -3,7 +3,52 @@ import { Context } from '../ContextProvider';
 import { ArrowUp } from "@phosphor-icons/react";
 import axios from 'axios';
 
-import loaderChat from '../assets/images/loader-chat.svg';
+
+const AnimatedCircles = () => {
+    return (
+        <svg
+            version="1.1"
+            id="L5"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="-20 40 110 20"
+            enableBackground="new 0 0 0 0"
+            xmlSpace="preserve"
+            className='w-10 h-10 text-base-content opacity-50'
+        >
+            <circle fill="currentColor" stroke="none" cx="6" cy="50" r="6">
+                <animateTransform
+                    attributeName="transform"
+                    dur="1s"
+                    type="translate"
+                    values="0 15 ; 0 -15; 0 15"
+                    repeatCount="indefinite"
+                    begin="0.1"
+                />
+            </circle>
+            <circle fill="currentColor" stroke="none" cx="30" cy="50" r="6">
+                <animateTransform
+                    attributeName="transform"
+                    dur="1s"
+                    type="translate"
+                    values="0 10 ; 0 -10; 0 10"
+                    repeatCount="indefinite"
+                    begin="0.2"
+                />
+            </circle>
+            <circle fill="currentColor" stroke="none" cx="54" cy="50" r="6">
+                <animateTransform
+                    attributeName="transform"
+                    dur="1s"
+                    type="translate"
+                    values="0 5 ; 0 -5; 0 5"
+                    repeatCount="indefinite"
+                    begin="0.3"
+                />
+            </circle>
+        </svg>
+    );
+};
 
 
 const ChatBot = () => {
@@ -100,18 +145,20 @@ const ChatBot = () => {
 
             <div className="h-full flex flex-col justify-end bg-base-300 rounded overflow-hidden">
 
-                <div className="chat-area p-3 rounded-lg overflow-auto">
+                <div className="chat-area p-3 rounded-lg overflow-auto border border-base-300">
 
                     {messages.map((message, index) => (
                         <div key={index} className={`chat w-full ${message.response === 'assistant' ? 'chat-start' : 'chat-end'}`}>
-                            <div className={`chat-bubble ${message.response === 'assistant' ? 'bg-base-100' : 'bg-base-200'}`}>{message.text}</div>
+                            <div className={`chat-bubble ${message.response === 'assistant' ? 'bg-base-100' : 'bg-base-200'}`}>
+                                <span className="text-sm text-base-content">{message.text}</span>
+                            </div>
                         </div>
                     ))}
 
                     {disabled &&
                         <div className="chat w-full chat-start">
                             <div className="chat-bubble bg-base-100 py-0 grid place-items-center">
-                                <img src={loaderChat} alt="Loading..." className='w-12 h-6 grid place-items-center opacity-30' />
+                                <AnimatedCircles />
                             </div>
                         </div>
                     }
@@ -120,10 +167,10 @@ const ChatBot = () => {
             <div className="pt-3 pb-8 bg-base-200">
                 <form action="" method="post" onSubmit={handleSend}>
                     <div className="w-full relative">
-                        <input type="text" placeholder="Type here" name="userInput" id="userInput" className="focus:outline-none input p-2 input-base w-full" disabled={disabled} />
+                        <input type="text" placeholder="Type here" name="userInput" id="userInput" className="focus:outline-none input p-2 input-base border border-base-300 w-full" disabled={disabled} />
 
                         <button className="bg-base-200 join-item absolute right-0 p-2.5 m-1 rounded tooltip tooltip-left" disabled={disabled} data-tip="Send Message">
-                            <span className='text-white/50'>
+                            <span className='text-base-content'>
                                 <ArrowUp size={20} />
                             </span>
                         </button>
